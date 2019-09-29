bff47c423838180742c4ce05a8aec8e054663f86
New <chauhanraunaq878@gmail.com>
2019-09-29 19:03:59 +0530

    components added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import Todos from './components/Todos'; 
+
 import './App.css';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "This is example",
+        isCompleted: false
+      },
+      {
+        id: 2,
+        title: "This is example 2",
+        isCompleted: false
+      },
+      {
+        id: 3,
+        title: "This is example 3",
+        isCompleted: false
+      }
+    ]
+  }
   render() {
+    //console.log(this.state.todos)
     return (
       <div className="App">
-        <h1>App</h1>
+        <Todos todos= { this.state.todos }/>
       </div>
     );
   }
